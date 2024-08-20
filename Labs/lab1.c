8d18a24540704b678d221ef0a863baf220c0780c
Illarionov Mykyta <120467710+Morloken@users.noreply.github.com>
2024-08-20 10:42:20 +0000

    Added main code for Lab1, added placeholder for Lab2

---
+++ Labs/lab1.c
@@ -0,0 +1,33 @@
+//number of variant - 10
+//Illarionov Mykyta
+
+// Користувач вводить число n. Визначити  факторіал числа n (n!).
+// Факторіал n вираховуються як добуток натуральних чисел від 1 до n;    n!=1 * 2 * 3 *…* n;    0!=1; 
+#include <iostream>
+using namespace std;
+
+long long factorial(int n) {
+    if (n == 0) return 1;  
+  
+    long long result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cout << "Input value of n(but only >0): ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Factorial is defined only for non-negative integers." << endl;
+    } else {
+        cout << "The factorial of " << n << " is: " << factorial(n) << endl;
+    }
+
+    return 0;
+}
+
+
