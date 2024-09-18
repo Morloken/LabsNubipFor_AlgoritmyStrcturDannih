6910f4be23df29988b8e92f6d1c83f7614029580
Illarionov Mykyta <120467710+Morloken@users.noreply.github.com>
2024-09-18 07:29:39 +0000

    Nothing important. Lab 3 completed

--- Labs/lab1.c
+++ Labs/lab1.c
@@ -9,7 +9,7 @@ using namespace std;
 long long factorial(int n) {
     if (n == 0) return 1;  
   
-    long long result = 1;
+    long long result = 1;   
     for (int i = 1; i <= n; ++i) {
         result *= i;
     }
